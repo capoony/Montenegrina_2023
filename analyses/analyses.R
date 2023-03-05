e09a0b7cc21cadda35d09b2eec943546391e6bc8
capoony <capoony@gmail.com>
2023-03-05 14:06:29 +0100

    update

--- analyses/analyses.R
+++ analyses/analyses.R
@@ -26,6 +26,7 @@ Colorcode$color[Colorcode$subclade %in% DATA$Subclade]
 ## replace Color Column based on Colorcode from Lisi
 DATA <- DATA %>%
     select(-Color) %>%
+    mutate(ID = str_replace(ID, "Montenegrina", "M.")) %>%
     left_join(Colorcode, by = c("Subclade" = "subclade"))
 
 ## focus on columns with Qualitative data and convert to factors
@@ -33,6 +34,8 @@ DATA.matrix <- as.data.frame(DATA[, 7:ncol(DATA) - 1], stringsAsFactors = TRUE)
 DATA$Num <- seq(1, nrow(DATA), 1)
 DATA$ID2 <- paste(DATA$ID, DATA$Num, sep = "_")
 
+DATA$Shape <- DATA$PCH
+DATA$Clade <- DATA$Color
 ## add rownames with unique IDs
 rownames(DATA.matrix) <- paste(DATA$ID, DATA$Num, sep = "_")
 
@@ -41,30 +44,43 @@ Hamming <- hammingdists(DATA.matrix)
 Hamming <- dist_setNames(
     Hamming, DATA$ID2
 )
+
+## Make Neighborjoining tree and root @ midpoint
 my_nj <- midpoint.root(nj(Hamming))
 
-rownames(DATA) <- DATA$ID2
+## rename labels and convert shape IDs to factors
 x <- full_join(as_tibble(my_nj), DATA, by = c("label" = "ID2"))
+x$Shape <- as.factor(x$Shape)
 tree2 <- as.treedata(x)
 
+## make lists with aesthetics for plotting colors and shapes
+
+## Clade = Color
 G <- list()
 for (i in seq(1, nrow(DATA), 1)) {
-    G[[DATA$Color[i]]] <- DATA$Color[i]
+    G[[DATA$Clade[i]]] <- DATA$Clade[i]
 }
 
+## Clade = Labels
 L <- list()
 for (i in seq(1, nrow(DATA), 1)) {
-    L[[DATA$Color[i]]] <- DATA$Subclade[i]
+    L[[DATA$Clade[i]]] <- DATA$Subclade[i]
 }
 
+## Shape symbols
+S <- list()
+for (i in seq(1, nrow(DATA), 1)) {
+    S[[as.character(DATA$Shape[i])]] <- DATA$Shape[i]
+}
+
+## plot tree
 PLOT.tree <- ggtree(tree2, layout = "roundrect") +
+    ## Set scales manually
     scale_color_manual(values = G, labels = L) +
     scale_fill_manual(values = G, labels = L) +
-    # geom_tiplab(aes(color = Color,
-    #     	label=label,
-    #         angle=0),
-    #     show.legend = FALSE
-    # ) +
+    scale_shape_manual(values = S) +
+
+    ## add tip labels
     geom_tiplab(
         aes(
             label = label,
@@ -73,17 +89,22 @@ PLOT.tree <- ggtree(tree2, layout = "roundrect") +
         hjust = -0.1,
         show.legend = FALSE
     ) +
-    geom_tippoint(aes(color = Color, fill = Color),
-        pch = DATA$PCH,
+
+    ## add colored symbols as tip-points according to clades
+    geom_tippoint(aes(color = Clade, fill = Clade, shape = Shape),
         size = 2,
         stroke = 1,
         show.legend = TRUE
     ) +
     theme_tree2() +
     theme_bw() +
-    xlim(0, 20)
+    xlim(0, 20) +
+    guides(
+        shape = guide_legend(override.aes = list(fill = "black"))
+    )
 PLOT.tree
 
+## save trees as PNG and PDF
 ggsave("H:/Work/Projects/HaringL/Montenegrina_2023/analyses/black_tree.pdf",
     PLOT.tree,
     width = 10,
