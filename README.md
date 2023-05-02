3a5fa6e45f4dec73c02e34cd8a89f625d73624d0
capoony <capoony@gmail.com>
2023-05-02 11:53:07 +0200

    update

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
-### Montenegrina NJ Tree based on qualitative morphological characterization
+### Montenegrina NJ Tree based on qualitative/quantitative morphological characterization
 
-Here, I constructed a distance matrix based on hamming distances across all 12 characters using the _cultevo_ package in _R_. After that, I used the Neighbor-Joining Method as implemented in the package _phangorn_ to reconstruct a tree and applied midpoint rooting using the package _phytools_ prior to plotting the tree with _ggtree_. 
-The raw input data can be found [here](data\Montenegrina\ qualitative_data\ copy.dat) and the R code for the analysis in [here](analyses/analyses.R). I recoded the shape descriptions to PCH parameters as used in R to encode different shapes.
+#### 1) Qualitative Data
 
-The resulting tree can be found here: 
+Here, I constructed a distance matrix based on hamming distances across all 12 qualitative characters using the _cultevo_ package in _R_. After that, I used the Neighbor-Joining Method as implemented in the package _phangorn_ to reconstruct a tree and applied midpoint rooting using the package _phytools_ prior to plotting the tree with _ggtree_. 
+The raw input data can be found [here](data\Montenegrina_rawdata.xlsx) and the R code for the analysis in [here](analyses/analyses.R). I recoded the shape descriptions to PCH parameters as used in R to encode different shapes.
 
-![tree](analyses/tree.png)
+The resulting tree can be found here for the complete dataset: 
 
-...and here is an circular version: 
+![tree](analyses/PLOT.Qual.full.tree.png)
 
-![tree](analyses/tree_circular.png)
-<!--  -->
+...and here for the D1/D2 subset:
+
+![tree](analyses/PLOT.Qual.d1d2.tree.png)
+
+
+#### 2) Quantitative Data
+Likewise, I repeated the same analyses for the 24 quantitative datasets using Euclidean distances
+
+The resulting tree can be found here for the complete dataset: 
+
+![tree](analyses/PLOT.Quant.full.tree.png)
+
+...and here for the D1/D2 subset:
+
+![tree](analyses/PLOT.Quant.d1d2.tree.png)
--- analyses/analyses.R
+++ analyses/analyses.R
@@ -6,151 +6,211 @@ library(treeio) ## as.treedata
 library(phytools) ## for midpoint rooting
 library(usedist) ## to add labels
 library(ggtree) ## for plotting "nice" trees
+library(readxl) ## read input
 
+## set working directory
 setwd("D:/GitHub/Montenegrina_2023/")
 
-## read Data
-DATA <- read.table("H:/Work/Projects/HaringL/Montenegrina_2023/data/Montenegrina qualitative_data copy.dat",
-    header = TRUE,
-    sep = "\t",
-    comment.char = ""
-)
+################## Functions ###################
 
-Colorcode <- read.table("H:/Work/Projects/HaringL/Montenegrina_2023/data/ColorCode.txt",
-    header = TRUE,
-    sep = "\t",
-    comment.char = ""
-)
+## 100X Bootstrapping with the ape package
+BOOT <- function(X, Y, B) {
+    f <- function(x) midpoint.root(nj(x))
+    tr <- f(X)
 
-Colorcode$color[Colorcode$subclade %in% DATA$Subclade]
+    ## do bootstrapping
+    bp <- boot.phylo(tr, as.matrix(X), f, B = B) / B
+    bp[is.na(bp)] <- paste0("<", 1 / B)
+    bp2 <- tibble(node = 1:Nnode(tr) + Ntip(tr), bootstrap = bp)
+    tr <- full_join(tr, bp2, by = "node")
 
-## replace Color Column based on Colorcode from Lisi
+    ## rename labels and convert shape IDs to factors
+    x <- full_join(as_tibble(tr), Y, by = c("label" = "ID2"))
+    x$Species <- as.factor(x$Species)
+    tr <- as.treedata(x, boot = bootstrap)
+
+    return(tr)
+}
+
+## create codes for plotting
+LISTs <- function(X) {
+    ## Clade = Color
+    C <- list()
+    for (i in seq(1, nrow(X), 1)) {
+        C[[X$Clade[i]]] <- X$Clade[i]
+    }
+
+    ## Clade = Labels
+    L <- list()
+    for (i in seq(1, nrow(X), 1)) {
+        L[[X$Clade[i]]] <- X$Subclade[i]
+    }
+
+    ## Clade = Labels
+    S <- list()
+    for (i in seq(1, nrow(X), 1)) {
+        S[[as.character(X$Species[i])]] <- X$Species[i]
+    }
+    return(list(C, L, S))
+}
+
+## plot tree
+TREE <- function(X, Y, Xlim, Xlab, width, height) {
+    ## get codes for plotting
+    LIST <- LISTs(Y)
+    Cf <- LIST[[1]]
+    Lf <- LIST[[2]]
+    Sf <- LIST[[3]]
+
+    ## plot tree
+    PLOT <- ggtree(X, layout = "roundrect") +
+        ## Set scales manually
+        scale_color_manual(values = Cf, labels = Lf) +
+        scale_fill_manual(values = Cf, labels = Lf) +
+        scale_shape_manual(values = Sf) +
+
+        ## add bootstrap
+        geom_text(aes(label = bootstrap),
+            hjust = 1.25,
+            vjust = -0.75,
+            size = 2,
+            col = "blue"
+        ) +
+
+        ## add tip labels
+        geom_tiplab(
+            aes(
+                label = label,
+                angle = 0
+            ),
+            hjust = -0.1,
+            size = 2,
+            show.legend = FALSE
+        ) +
+
+        ## add colored symbols as tip-points according to clades
+        geom_tippoint(aes(color = Clade, fill = Clade, shape = Species),
+            size = 2,
+            stroke = 0.75,
+            show.legend = FALSE
+        ) +
+        theme_tree2() +
+        theme_bw() +
+        xlim(0, Xlim) +
+        xlab(Xlab) +
+        ylab("Number of Individuals")
+
+    ## save trees as PNG and PDF
+    PDF <- paste0("analyses/PLOT.", deparse(substitute(X)), ".pdf")
+    ggsave(PDF,
+        PLOT,
+        width = width,
+        height = height
+    )
+    PNG <- paste0("analyses/PLOT.", deparse(substitute(X)), ".png")
+    ggsave(PNG,
+        PLOT,
+        width = width,
+        height = height
+    )
+}
+
+
+####################### Code ###################
+
+## read dataset
+DATA <- read_excel("data/Montenegrina_rawdata.xlsx")
+
+## rename
 DATA <- DATA %>%
-    select(-Color) %>%
-    mutate(ID = str_replace(ID, "Montenegrina", "M.")) %>%
-    left_join(Colorcode, by = c("Subclade" = "subclade"))
+    rename(
+        Clade = CladeColor,
+        ID2 = ID_short,
+        Species = SpeciesShape
+    )
 
-## focus on columns with Qualitative data and convert to factors
-DATA.matrix <- as.data.frame(DATA[, 5:ncol(DATA) - 2], stringsAsFactors = TRUE)
-DATA$Num <- seq(1, nrow(DATA), 1)
-DATA$ID2 <- paste(DATA$ID, DATA$Num, sep = "_")
+## restrict to Measurment columns
+DATA.matrix <- as.data.frame(DATA[, 6:ncol(DATA)])
+rownames(DATA.matrix) <- DATA$ID2
 
-DATA$Shape <- DATA$PCH
-DATA$Clade <- DATA$Color
-## add rownames with unique IDs
-rownames(DATA.matrix) <- paste(DATA$ID, DATA$Num, sep = "_")
+## Qual/Quant Data
+Quant.full <- DATA.matrix[, 1:24]
+Qual.full <- DATA.matrix[, 25:ncol(DATA.matrix)]
+
+## subset D1 D2
+DATA.d1d2 <- DATA %>%
+    filter(Subclade %in% c("D1", "D2"))
+
+DATA.matrix.d1d2 <- as.data.frame(DATA.d1d2[, 6:ncol(DATA)])
+rownames(DATA.matrix.d1d2) <- DATA.d1d2$ID2
+
+Quant.d1d2 <- DATA.matrix.d1d2[, 1:24]
+Qual.d1d2 <- DATA.matrix.d1d2[, 25:ncol(DATA.matrix)]
+
+################ Analysis of Full Dataset ###################
+
+## Make Neighborjoining tree based on euclidean distance and root @ midpoint
+Quant.full.dist <- dist(Quant.full)
+
+## make NJ tree with bootstrapping
+Quant.full.tree <- BOOT(Quant.full.dist, DATA, 100)
+
+## plot tree
+TREE(
+    X = Quant.full.tree,
+    Y = DATA,
+    Xlim = 80,
+    Xlab = "Euclidean Distance",
+    width = 10,
+    height = 25
+)
 
 ## calculate Hamming Distances
-Hamming <- hammingdists(DATA.matrix)
-Hamming <- dist_setNames(
-    Hamming, DATA$ID2
+Qual.full.dist <- hammingdists(Qual.full)
+Qual.full.dist <- dist_setNames(
+    Qual.full.dist, DATA$ID2
 )
 
-## Make Neighborjoining tree and root @ midpoint
-my_nj <- midpoint.root(nj(Hamming))
-
-## rename labels and convert shape IDs to factors
-x <- full_join(as_tibble(my_nj), DATA, by = c("label" = "ID2"))
-x$Shape <- as.factor(x$Shape)
-tree2 <- as.treedata(x)
-
-## make lists with aesthetics for plotting colors and shapes
-
-## Clade = Color
-G <- list()
-for (i in seq(1, nrow(DATA), 1)) {
-    G[[DATA$Clade[i]]] <- DATA$Clade[i]
-}
-
-## Clade = Labels
-L <- list()
-for (i in seq(1, nrow(DATA), 1)) {
-    L[[DATA$Clade[i]]] <- DATA$Subclade[i]
-}
-
-## Shape symbols
-S <- list()
-for (i in seq(1, nrow(DATA), 1)) {
-    S[[as.character(DATA$Shape[i])]] <- DATA$Shape[i]
-}
+## make NJ tree with bootstrapping
+Qual.full.tree <- BOOT(Qual.full.dist, DATA, 100)
 
 ## plot tree
-PLOT.tree <- ggtree(tree2, layout = "roundrect") +
-    ## Set scales manually
-    scale_color_manual(values = G, labels = L) +
-    scale_fill_manual(values = G, labels = L) +
-    scale_shape_manual(values = S) +
-
-    ## add tip labels
-    geom_tiplab(
-        aes(
-            label = label,
-            angle = 0
-        ),
-        hjust = -0.1,
-        size = 2,
-        show.legend = FALSE
-    ) +
-
-    ## add colored symbols as tip-points according to clades
-    geom_tippoint(aes(color = Clade, fill = Clade, shape = Shape),
-        size = 1,
-        stroke = 1,
-        show.legend = TRUE
-    ) +
-    theme_tree2() +
-    theme_bw() +
-    xlim(0, 10) +
-    guides(
-        shape = guide_legend(override.aes = list(fill = "black"))
-    )
-# PLOT.tree
-
-## save trees as PNG and PDF
-ggsave("H:/Work/Projects/HaringL/Montenegrina_2023/analyses/tree.pdf",
-    PLOT.tree,
+TREE(
+    X = Qual.full.tree,
+    Y = DATA,
+    Xlim = 8.5,
+    Xlab = "Hamming Distance",
     width = 10,
-    height = 20
-)
-
-ggsave("H:/Work/Projects/HaringL/Montenegrina_2023/analyses/tree.png",
-    PLOT.tree,
-    width = 10,
-    height = 20
-)
-
-## plot tree
-PLOT.tree <- ggtree(tree2, layout = "circular") +
-    ## Set scales manually
-    scale_color_manual(values = G, labels = L) +
-    scale_fill_manual(values = G, labels = L) +
-    scale_shape_manual(values = S) +
-
-    ## add tip labels
-    geom_tiplab2() +
-
-    ## add colored symbols as tip-points according to clades
-    geom_tippoint(aes(color = Clade, fill = Clade, shape = Shape),
-        size = 2,
-        stroke = 1,
-        show.legend = TRUE
-    ) +
-    theme_tree2() +
-    theme_bw() +
-    guides(
-        shape = guide_legend(override.aes = list(fill = "black"))
-    )
-PLOT.tree
-
-## save trees as PNG and PDF
-ggsave("H:/Work/Projects/HaringL/Montenegrina_2023/analyses/tree_circular.pdf",
-    PLOT.tree,
-    width = 25,
     height = 25
 )
 
-ggsave("H:/Work/Projects/HaringL/Montenegrina_2023/analyses/tree_circular.png",
-    PLOT.tree,
-    width = 25,
-    height = 25
+############## D1/D2 only
+
+## Make Neighborjoining tree based on euclidean distance and root @ midpoint
+Quant.d1d2.dist <- dist(Quant.d1d2)
+Quant.d1d2.tree <- BOOT(Quant.d1d2.dist, DATA.d1d2, 100)
+
+TREE(
+    X = Quant.d1d2.tree,
+    Y = DATA.d1d2,
+    Xlim = 45,
+    Xlab = "Euclidean Distance",
+    width = 5,
+    height = 7
+)
+
+## calculate Hamming Distances and make
+Qual.d1d2.dist <- hammingdists(Qual.d1d2)
+Qual.d1d2.dist <- dist_setNames(
+    Qual.d1d2.dist, DATA.d1d2$ID2
+)
+Qual.d1d2.tree <- BOOT(Qual.d1d2.dist, DATA.d1d2, 100)
+
+TREE(
+    X = Qual.d1d2.tree,
+    Y = DATA.d1d2,
+    Xlim = 8.5,
+    Xlab = "Hamming Distance",
+    width = 5,
+    height = 7
 )
