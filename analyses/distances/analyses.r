d0a9274fe528de7195add0a80e1809e11a1aa80b
capoony <capoony@gmail.com>
2023-10-25 17:09:43 +0200

    updazte

--- analyses/distances/analyses.r
+++ analyses/distances/analyses.r
@@ -1,17 +1,19 @@
 library(tidyverse)
 library(pheatmap)
-library(ecodist, help, pos = 2, lib.loc = NULL)
+library(ecodist)
 
 ### read Data
 DATA<-read.table("/media/inter/mkapun/projects/Montenegrina_2023/data/distance.tsv",
 header=T,
 na.string="NA")
 
-## 
+## convert to distance matrices
 Geo<-as.dist(xtabs(DATA[, 3] ~ DATA[, 2] + DATA[, 1]))
 DNA<-as.dist(xtabs(DATA[, 4] ~ DATA[, 2] + DATA[, 1]))
 Morph<-as.dist(xtabs(DATA[, 5] ~ DATA[, 2] + DATA[, 1]))
 
+
+## do Mantel tests in all combinations 
 sink("/media/inter/mkapun/projects/Montenegrina_2023/analyses/distances/distance.stat")
 print("DNA vs. Geo")
 mantel(DNA ~ Geo,nperm=1000000)
@@ -19,10 +21,9 @@ print("Morph vs. DNA")
 mantel(Morph ~ DNA,nperm=1000000)
 print("Morph vs. Geo")
 mantel(Morph ~Geo,nperm=1000000)
-#mantel(Morph ~Geo+DNA,nperm=10000)
 sink()
 
-
+## plot heatmaps
 pdf("/media/inter/mkapun/projects/Montenegrina_2023/analyses/distances/distance_DNA.pdf")
 pheatmap(as.matrix(DNA))
 dev.off()
