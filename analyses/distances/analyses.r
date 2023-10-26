7242bd0fb1290bcbed3175ea0a87cc6d9ef3b705
capoony <capoony@gmail.com>
2023-10-26 12:14:36 +0200

    update

--- analyses/distances/analyses.r
+++ analyses/distances/analyses.r
@@ -33,3 +33,32 @@ dev.off()
 pdf("/media/inter/mkapun/projects/Montenegrina_2023/analyses/distances/distance_Morph.pdf")
 pheatmap(as.matrix(Morph))
 dev.off()
+
+
+## plot pairwise regression plots
+reg <- function(x, y, col) abline(lm(y~x), col=col) 
+
+panel.lm =  function (x, y, col = par("col"), bg = NA, pch = par("pch"), 
+    cex = 1, col.smooth = "red", span = 2/3, iter = 3, ...)  {
+    points(x, y, pch = pch, col = col, bg = bg, cex = cex)
+    ok <- is.finite(x) & is.finite(y)
+    if (any(ok)) reg(x[ok], y[ok], col.smooth)
+}
+
+panel.cor <- function(x, y, digits = 2, prefix = "R²: ", cex.cor, ...)
+{
+ usr <- par("usr"); on.exit(par(usr))
+ par(usr = c(0, 1, 0, 1))
+ r <- abs(cor(x, y))**2
+ txt <- format(c(r, 0.123456789), digits = digits)[1]
+ txt <- paste0(prefix, txt)
+ text(0.5, 0.5, txt, cex = 1.1, font = 4)
+ }
+
+pdf("/media/inter/mkapun/projects/Montenegrina_2023/analyses/distances/PairwiseRegression.pdf")
+
+pairs(DATA[,c(5,4,3)], panel = panel.lm,
+    cex = 1.5, pch = 19, col = adjustcolor(4, .4), cex.labels = 2, 
+    font.labels = 2, lower.panel = panel.cor)
+
+dev.off()
